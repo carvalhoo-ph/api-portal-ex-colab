cb8d7a582a966928771d003c8fd93be7285f3b37
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-01-22 22:27:54 -0300

    Implantação 02

--- iam.tf
+++ iam.tf
@@ -1,1 +1,53 @@
-// Arquivo não necessário no momento
+resource "aws_iam_role" "github_actions" {
+  name = "github_actions_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name = "github_actions_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "apigateway:POST",
+          "apigateway:GET",
+          "apigateway:PUT",
+          "apigateway:DELETE",
+          "apigateway:PATCH",
+          "apigateway:HEAD",
+          "apigateway:OPTIONS"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_attach" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
