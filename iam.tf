d75cd25e9f796265052630da1b87edb1ff6e07ef
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-01-23 00:30:17 -0300

    Implantação 08

--- iam.tf
+++ iam.tf
@@ -62,6 +62,47 @@ resource "aws_iam_role_policy_attachment" "github_actions_attach" {
   policy_arn = aws_iam_policy.github_actions_policy.arn
 }
 
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role_${random_string.suffix.result}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_exec_policy" {
+  name = "lambda_exec_policy_${random_string.suffix.result}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_exec_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_exec_policy.arn
+}
+
 resource "random_string" "suffix" {
   length  = 8
   special = false
