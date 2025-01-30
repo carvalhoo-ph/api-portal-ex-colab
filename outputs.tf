44f291315b5f912a75b0e7d7975cdd0e96f6d7bb
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-01-30 19:50:07 -0300

    Atualiza 04

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "api_url" {
   description = "The URL of the API Gateway"
   value       = aws_api_gateway_rest_api.api.execution_arn
 }
+
+output "api_invoke_url" {
+  description = "The URL to invoke the API Gateway"
+  value       = aws_api_gateway_rest_api.api.invoke_url
+}
