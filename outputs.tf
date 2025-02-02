5f07186053cc4f54b71f6205e16f9ec9357759b8
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-02-02 19:58:55 -0300

    Teste de Carga 03

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "api_gateway_url" {
-  value       = data.aws_api_gateway_rest_api.api.execution_arn
-  description = "The execution ARN of the API Gateway"
-}
-
 output "api_invoke_url" {
   value       = "https://${data.aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/prod"
   description = "The URL of the API Gateway"
