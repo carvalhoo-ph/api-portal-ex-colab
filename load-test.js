1ac6ad6f1c6453f2398837379bfda9d2f604db65
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-02-01 23:47:18 -0300

    teste carga 02

--- load-test.js
+++ load-test.js
@@ -11,17 +11,17 @@ export let options = {
 
 export default function () {
   // Testar o endpoint /login
-  let resLogin = http.get('https://<your-api-gateway-url>/login');
+  let resLogin = http.get('https://4fr9f330c6.execute-api.us-east-1.amazonaws.com/prod/login');
   check(resLogin, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 
   // Testar o endpoint /periodo-demonstrativo
-  let resPeriodoDemonstrativo = http.get('https://<your-api-gateway-url>/periodo-demonstrativo');
+  let resPeriodoDemonstrativo = http.get('https://4fr9f330c6.execute-api.us-east-1.amazonaws.com/prod/periodo-demonstrativo');
   check(resPeriodoDemonstrativo, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 
   // Testar o endpoint /demonstrativo-pgto
-  let resDemonstrativoPgto = http.get('https://<your-api-gateway-url>/demonstrativo-pgto');
+  let resDemonstrativoPgto = http.get('https://4fr9f330c6.execute-api.us-east-1.amazonaws.com/prod/demonstrativo-pgto');
   check(resDemonstrativoPgto, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
