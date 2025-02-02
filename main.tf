95c84d28590c290cb39c06c4d6709edbbfe89352
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-02-02 17:49:04 -0300

    teste 03

--- main.tf
+++ main.tf
@@ -325,5 +325,5 @@ resource "aws_api_gateway_method_response" "method_response_options_demonstrativ
 }
 
 output "api_gateway_url" {
-  value = aws_api_gateway_rest_api.api.invoke_url
+  value = aws_api_gateway_rest_api.api.execution_arn
 }
