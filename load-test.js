14fc9c5b2bfab28ff8071456bab43e3c43f70047
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-02-01 23:11:44 -0300

    teste carga 01

---
+++ load-test.js
@@ -0,0 +1,27 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 20 }, // ramp up to 20 users
+    { duration: '10m', target: 20 },  // stay at 20 users for 1 minute
+    { duration: '30s', target: 0 },  // ramp down to 0 users
+  ],
+};
+
+export default function () {
+  // Testar o endpoint /login
+  let resLogin = http.get('https://<your-api-gateway-url>/login');
+  check(resLogin, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+
+  // Testar o endpoint /periodo-demonstrativo
+  let resPeriodoDemonstrativo = http.get('https://<your-api-gateway-url>/periodo-demonstrativo');
+  check(resPeriodoDemonstrativo, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+
+  // Testar o endpoint /demonstrativo-pgto
+  let resDemonstrativoPgto = http.get('https://<your-api-gateway-url>/demonstrativo-pgto');
+  check(resDemonstrativoPgto, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
