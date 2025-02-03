358542dd0f44cd74c53e0a1d256268e8f4b74748
carvalhoo-ph <149440383+carvalhoo-ph@users.noreply.github.com>
2025-02-02 21:43:28 -0300

    teste carga 03

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,1 @@
 // Remova esta definição duplicada, pois já existe em main.tf
-// output "api_gateway_url" {
-//   value       = "https://${local.api_id}.execute-api.${var.region}.amazonaws.com/prod"
-//   description = "The URL of the API Gateway"
-// }
